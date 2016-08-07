b5d8aff0d67b4dc083ca66db218df161827bcdf2
root <root@localhost.localdomain>
2016-08-07 20:25:12 -0300

    Imagens da segmentação e inclusão da apresentação

--- codigoSegmentacao.cpp
+++ codigoSegmentacao.cpp
@@ -4,55 +4,70 @@ using namespace std;
 using namespace cv;
 int main(int, char** argv)
 {
-    // Load the image
+    // Carrega a imagem - com argumentos do Bash
     Mat src = imread(argv[1]);
-    // Check if everything was fine
+	// Checagem se a imagem foi carregada
     if (!src.data)
         return -1;
-    // Show source image
-    imshow("Source Image", src);
-    // Change the background from white to black, since that will help later to extract
-    // better results during the use of Distance Transform
-    //for( int x = 0; x < src.rows; x++ ) {
-     // for( int y = 0; y < src.cols; y++ ) {
-     //     if ( src.at<Vec3b>(x, y) == Vec3b(255,255,255) ) {
-     //       src.at<Vec3b>(x, y)[0] = 0;
-     //       src.at<Vec3b>(x, y)[1] = 0;
-     //       src.at<Vec3b>(x, y)[2] = 0;
-     //     }
-     //   }
-    //}
-    // Show output image
-    //imshow("Black Background Image", src);
-    // Create a kernel that we will use for accuting/sharpening our image
+    // Mostra a imagem original
+    imshow("Imagem geral", src);
+
+ //Embaçamento da imagem para diminuir detalhes
+	Mat dst1; 
+	for (int i=1; i<51; i=i+2){ 
+      //smooth the image in the "src" and save it to "dst"
+      blur(src, dst1, Size(i,i));
+}
+      //show the blurred image with the text
+      imshow( "Smoothing by avaraging", dst1 );
+src = dst1;
+   
+   // Aqui definimos uma máscara correspondente a derivada segunda para o filtro Laplaciano
+   //Estudar: Converte em algo "mais profundo" do que CV_8U (???) porque o kernel tem alguns valores negativos então o Laplaciano terá valores negativos que serão truncados ao usarmos unsigned int   
     Mat kernel = (Mat_<float>(3,3) <<
             1,  1, 1,
             1, -8, 1,
-            1,  1, 1); // an approximation of second derivative, a quite strong kernel
-    // do the laplacian filtering as it is
-    // well, we need to convert everything in something more deeper then CV_8U
-    // because the kernel has some negative values,
-    // and we can expect in general to have a Laplacian image with negative values
-    // BUT a 8bits unsigned int (the one we are working with) can contain values from 0 to 255
-    // so the possible negative number will be truncated
+            1,  1, 1); 
     Mat imgLaplacian;
-    Mat sharp = src; // copy source image to another temporary one
-    filter2D(sharp, imgLaplacian, CV_32F, kernel);
-    src.convertTo(sharp, CV_32F);
-    Mat imgResult = sharp - imgLaplacian;
+    Mat sharp = src; // copia da imagem original
+	
+	//CV_8U
+	//CV_32F
+	//CV_8UC3
+	
+    //ler referência
+    //filter2D(sharp, imgLaplacian, CV_32F, kernel);
+    //src.convertTo(sharp, CV_32F);
+    //Mat imgResult = sharp - imgLaplacian;
+
     // convert back to 8bits gray scale
-    imgResult.convertTo(imgResult, CV_8UC3);
-    imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
-    // imshow( "Laplace Filtered Image", imgLaplacian );
-    imshow( "New Sharped Image", imgResult );
-    src = imgResult; // copy back
+    //imgResult.convertTo(imgResult, CV_8UC3);
+    //imgLaplacian.convertTo(imgLaplacian, CV_8UC3);
+    //imshow( "Laplace Filtered Image", imgLaplacian );
+    //imshow( "New Sharped Image", imgResult );
+    //src = imgResult; // copy back
     // Create binary image from source image
     Mat bw;
+//ler referência
     cvtColor(src, bw, CV_BGR2GRAY);
     threshold(bw, bw, 40, 255, CV_THRESH_BINARY | CV_THRESH_OTSU);
-    imshow("Binary Image", bw);
+Mat LaplacianoBinario;
+
+filter2D(bw, LaplacianoBinario, CV_32F, kernel);
+bw.convertTo(bw, CV_32F);
+Mat ret = bw - LaplacianoBinario;
+ret.convertTo(ret, CV_8UC3);
+bw.convertTo(bw,CV_8UC3);
+LaplacianoBinario.convertTo(LaplacianoBinario, CV_8UC3);
+    //imshow( "Laplace Filtered Image", LaplacianoBinario );
+	//imshow("Binario - Lapaciano",ret);
+    //imshow("Binary Image", bw);
+//src = ret;
+
+
     // Perform the distance transform algorithm
     Mat dist;
+//ler muito
     distanceTransform(bw, dist, CV_DIST_L2, 3);
     // Normalize the distance image for range = {0.0, 1.0}
     // so we can visualize and threshold it
@@ -60,11 +75,12 @@ int main(int, char** argv)
     imshow("Distance Transform Image", dist);
     // Threshold to obtain the peaks
     // This will be the markers for the foreground objects
-    threshold(dist, dist, .2, 1., CV_THRESH_BINARY);
+    threshold(dist, dist, .59, 1., CV_THRESH_BINARY);
     // Dilate a bit the dist image
     Mat kernel1 = Mat::ones(3, 3, CV_8UC1);
+//um pouco de morfologia pra que??
     dilate(dist, dist, kernel1);
-    imshow("Peaks", dist);
+    imshow("Picos", dist);
     // Create the CV_8U version of the distance image
     // It is needed for findContours()
     Mat dist_8u;
@@ -80,7 +96,13 @@ int main(int, char** argv)
     // Draw the background marker
     circle(markers, Point(5,5), 3, CV_RGB(255,255,255), -1);
     imshow("Markers", markers*10000);
+
+
     // Perform the watershed algorithm
+//ret.convertTo(ret, CV_8UC1);
+	
+Mat water = Mat::zeros(markers.size(), src.type());
+printf("O Tipo de src é %c. \n", src.type());
     watershed(src, markers);
     Mat mark = Mat::zeros(markers.size(), CV_8UC1);
     markers.convertTo(mark, CV_8UC1);
@@ -115,7 +137,7 @@ int qtdSegmentos = 0;
     }
 	qtdSegmentos = colors.size();
     // Visualize the final image
-    printf("Temos %d Bolinhas :)",qtdSegmentos); 
+    printf("Temos %d Bolinhas. \n",qtdSegmentos); 
     imshow("Final Result", dst);
 
     waitKey(0);
